e64096736f9af3aecd590d3b3808c52f01a8978c
abhishek <abhishekanandsm0311@gmail.com>
2024-12-19 13:42:26 +0530

    merge sort

--- array-mergesort.cpp
+++ array-mergesort.cpp
@@ -42,3 +42,12 @@ void merge(vector<int>& arr, int left, int mid, int right){
   }
 }
 
+void mergeSort(vector<int>& arr, int left, int right){
+  if(left>=right){
+    return;
+  }
+  int mid = left + (right-left)/2;
+  mergeSort(arr, left, mid);
+  mergeSort(arr,mid+1, right);
+  merge(arr,left, mid, right);
+}
