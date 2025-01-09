212c7843646d848aa65f847ababb7bc5c33060d1
abhishek <abhishekanandsm0311@gmail.com>
2025-01-09 12:56:54 +0530

    hashamp approach using only one for loop

--- two-sum.cpp
+++ two-sum.cpp
@@ -30,3 +30,19 @@ public:
         return {};
     }
 };
+
+//hashmap using only one for loop
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> umap;
+        for(int i=0;i<nums.size();i++){
+            int complement = target - nums[i];
+            if(umap.find(complement)!=umap.end()){
+                return {umap[complement],i};
+            }
+            umap[nums[i]]=i;
+        }
+        return {};
+    }
+};
